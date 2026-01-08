4863e3571c7f4f8918c850f58051727ceba0e251
Runhua Zhang <riva.rh.zhang@gmail.com>
2026-01-09 00:54:07 +0800

    Fix organizers grid and ignore Jekyll build output

--- _site/assets/main.js
+++
@@ -1,8 +0,0 @@
-// Main JS for feminist-hci site
-
-document.addEventListener("DOMContentLoaded", () => {
-  // AnchorJS: add link icons to headings (if the library is loaded)
-  if (window.anchors && typeof window.anchors.add === "function") {
-    window.anchors.add();
-  }
-});
